c097d62bb9630aac758ca4695d409624001b19a0
LeandroLDomingos <leandrolucasdomingos15@outlook.com>
2024-05-02 18:39:52 -0300

    first

---
+++ instituto-quadril/resources/views/welcome.blade.php
@@ -0,0 +1,376 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Instituo Quadril</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+
+    <link href="css/templatemo-medic-care.css" rel="stylesheet">
+    <!--
+
+TemplateMo 566 Instituto Quadril
+
+https://templatemo.com/tm-566-medic-care
+
+-->
+</head>
+
+<body id="top">
+
+    <main>
+
+        <nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+            <div class="container">
+                <a class="navbar-brand mx-auto d-lg-none" href="{{ route('home') }}">
+                    Instituto Quadril
+                    <strong class="d-block">Especialista em Ortopedia</strong>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#hero">Inicio</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">Sobre</a>
+                        </li>
+
+                        <a class="navbar-brand d-none d-lg-block" href="{{ route('home') }}">
+                            Instituto Quadril
+                            <strong class="d-block">Especialista em Ortopedia</strong>
+                        </a>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#reviews">Doenças</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+
+        <section class="hero" id="hero">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12">
+                        <div id="myCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="images/slider/portrait-successful-mid-adult-doctor-with-crossed-arms.jpg"
+                                        class="img-fluid" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img src="images/slider/young-asian-female-dentist-white-coat-posing-clinic-equipment.jpg"
+                                        class="img-fluid" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img src="images/slider/doctor-s-hand-holding-stethoscope-closeup.jpg"
+                                        class="img-fluid" alt="">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="heroText d-flex flex-column justify-content-center">
+
+                            <h1 class="mt-auto mb-2">
+                                <div class="animated-info">
+                                    <span class="animated-item">Equipe Especializada</span>
+                                    <span class="animated-item">Cuidado Personalizado</span>
+                                </div>
+                            </h1>
+
+                            <p class="mb-4">Bem-vindo ao Instituto Quadril, seu destino confiável para cuidados
+                                ortopédicos de qualidade. No Instituto Quadril, estamos comprometidos em proporcionar
+                                excelência em tratamentos ortopédicos, especialmente no cuidado do quadril.</p>
+
+                            <div class="heroLinks d-flex flex-wrap align-items-center">
+                                <a class="custom-link me-4" href="#about" data-hover="Leia Mais">Leia Mais</a>
+
+                                <a type="button" class="btn btn-success" href="https://wa.me/553197121111"
+                                    target=_blank>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                        <path
+                                            d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232">
+                                        </path>
+                                    </svg>
+                                    Entre em contato
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="section-padding" id="about">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <h2 class="mb-lg-3 mb-3">Dr. Leandro Emilio</h2>
+
+                        <p>O Instituto Quadril é um renomado consultório ortopédico dedicado ao tratamento de uma
+                            variedade de condições relacionadas ao quadril. Nossa equipe é composta por ortopedistas
+                            altamente qualificados, especializados no diagnóstico, tratamento e reabilitação de
+                            problemas do quadril.
+                        </p>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-5 col-12 mx-auto">
+                        <div
+                            class="featured-circle bg-white shadow-lg d-flex justify-content-center align-items-center">
+                            <p class="featured-text"><span class="featured-number">9</span> Anos<br> de Experiência
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="gallery">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-6 ps-0">
+                        <img src="images/gallery/medium-shot-man-getting-vaccine.jpg" class="img-fluid galleryImage"
+                            alt="get a vaccine" title="get a vaccine for yourself">
+                    </div>
+
+                    <div class="col-lg-6 col-6 pe-0">
+                        <img src="images/gallery/female-doctor-with-presenting-hand-gesture.jpg"
+                            class="img-fluid galleryImage" alt="wear a mask" title="wear a mask to protect yourself">
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section class="section-padding pb-0" id="reviews">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12">
+                        <h2 class="text-center mb-lg-5 mb-4">Doenças</h2>
+
+
+                        <div class="container">
+                            <hr>
+                            <div class="row">
+
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <h2 class="mb-lg-3 mb-3">Artrose do Quadril</h2>
+
+                                    <p>Também conhecida como osteoartrite, é uma condição degenerativa das articulações
+                                        que causa dor, rigidez e perda de movimento no quadril.
+                                    </p>
+
+                                </div>
+
+                                <div class="col-lg-4 col-md-5 col-12 mx-auto d-flex justify-content-center">
+                                    <div
+                                        class="featured-circle bg-white shadow-lg d-flex justify-content-center align-items-center">
+                                        <img src="images/doencas/artrose_do_quadril.jpg" alt=""
+                                            class="img-fluid rounded-circle"
+                                            style="width: 100%; height: 100%; object-fit: cover;">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <br>
+                        <hr>
+                        <div class="container">
+                            <div class="row">
+
+                                <div class="col-lg-4 col-md-5 col-12 mx-auto d-flex justify-content-center">
+                                    <div
+                                        class="featured-circle bg-white shadow-lg d-flex justify-content-center align-items-center">
+                                        <img src="images/doencas/bursite.jpg" alt=""
+                                            class="img-fluid rounded-circle"
+                                            style="width: 100%; height: 100%; object-fit: cover;">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <h2 class="mb-lg-3 mb-3">Bursite Trocantérica</h2>
+
+                                    <p>É a inflamação da bursa trocantérica, uma pequena bolsa cheia de líquido
+                                        localizada na parte externa do quadril. Causa dor na lateral do quadril e pode
+                                        ser agravada pela atividade.
+                                    </p>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <br>
+                        <hr>
+                        <div class="container">
+                            <div class="row">
+
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <h2 class="mb-lg-3 mb-3">Impacto Femoroacetabula</h2>
+
+                                    <p> É uma condição em que há um contato anormal entre o fêmur e o acetábulo (a
+                                        cavidade do quadril), causando dor, rigidez e eventual desgaste da articulação.
+                                    </p>
+
+                                </div>
+
+                                <div class="col-lg-4 col-md-5 col-12 mx-auto d-flex justify-content-center">
+                                    <div
+                                        class="featured-circle bg-white shadow-lg d-flex justify-content-center align-items-center">
+                                        <img src="images/doencas/femoroacetabula.jpeg" alt=""
+                                            class="img-fluid rounded-circle"
+                                            style="width: 100%; height: 100%; object-fit: cover;">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="site-footer section-padding" id="contact">
+        <h2 class="text-center mb-lg-5 mb-4">Como chegar</h2>
+        <div class="row">
+            <div class="container text-center">
+                <div class="card w-50 p-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Encontre pelo MAPS</h5>
+                    </div>
+                    <div class="ratio ratio-1x1">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.074780451583!2d-43.96161992388547!3d-19.92125333803793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa69741bd3cc4f9%3A0x8ea06e3faffc31e8!2sR.%20Jaceguai%2C%20208%20-%20Prado%2C%20Belo%20Horizonte%20-%20MG%2C%2030411-040!5e0!3m2!1spt-BR!2sbr!4v1714165914366!5m2!1spt-BR!2sbr"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <br>
+                    <a type="button" class="btn btn-dark"
+                    href="https://m.uber.com/ul/?action=setPickup&pickup=my_location&dropoff[formatted_address]=Instituto%20do%20Quadril&dropoff[latitude]=-19.920188791201795&dropoff[longitude]=-43.9591737460263">
+                    Uber <i class="fab fa-uber"></i></a>
+                </div>
+
+            </div>
+        </div>
+        <br>
+        <hr>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 me-auto col-12">
+                    <h5 class="mb-lg-4 mb-3">Horário de Funcionamento</h5>
+
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex">
+                            Domingo : Fechado
+                        </li>
+
+                        <li class="list-group-item d-flex">
+                            Segunda, Terça - Sexta
+                            <span>8:00 - 13:30 </span>
+                        </li>
+
+                        <li class="list-group-item d-flex">
+                            Sábado
+                            <span>10:30 - 17:30 </span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 col-12 my-4 my-lg-0">
+                    <h5 class="mb-lg-4 mb-3">Nossa Clínica</h5>
+
+                    <p><a href="mailto:hello@company.co">hello@company.co</a>
+                    <p>
+
+                    <p>R. Jaceguai, 208 - Sala 614 - Prado</p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-12 ms-auto">
+                    <h5 class="mb-lg-4 mb-2">Redes Sociais</h5>
+
+                    <ul class="social-icon">
+                        <li><a href="#" class="social-icon-link bi-facebook"></a></li>
+
+                        <li><a href="https://wa.me/553197121111" class="social-icon-link bi-whatsapp"></a></li>
+
+                        <li><a href="https://www.instagram.com/institutoquadril?igsh=MThrdnFoM3U0aXhmbA%3D%3D&utm_source=qr"
+                                class="social-icon-link bi-instagram"></a></li>
+
+                    </ul>
+
+                    <div>
+                        <p class="copyright-text">Copyright © Instituto Quadril 2021
+                            <br><br>Design: <a href="" target="_parent">Leandro Lucas</a>
+                        </p>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+            </section>
+    </footer>
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/scrollspy.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <!--
+
+TemplateMo 566 Instituto Quadril
+
+https://templatemo.com/tm-566-medic-care
+
+-->
+</body>
+
+</html>
